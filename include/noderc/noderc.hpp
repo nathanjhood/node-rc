a165505ea322013395fb887a55ef16268dfb1ff9
StoneyDSP <nathanjhood@googlemail.com>
2024-01-10 02:49:46 +0100

    Began NodeRC header
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/noderc/noderc.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file noderc.hpp
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @brief
+ * @version 1.0.0
+ * @date 2024-01-10
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef NODERC_H_
+#define NODERC_H_
+
+#endif // NODERC_H_
