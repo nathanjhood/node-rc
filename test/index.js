ff11d2e3a535d9d9f1b2688d0ed0aa3e9bb4e1e0
StoneyDSP <nathanjhood@googlemail.com>
2024-01-11 02:02:59 +0100

    Improved initial tests
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/index.js
+++ test/index.js
@@ -3,13 +3,45 @@ const noderc = require("../lib/noderc.node");
 console.log(noderc.hello());
 console.log(noderc.version());
 
-console.log("Does NodeRC contain a file named 'tst.txt'? = %s",   noderc.isFile("tst.txt"));
-console.log("Does NodeRC contain a file named 'sts.xtx'? = %s",   noderc.isFile("sts.xtx"));
+function doTest(tst) {
 
-console.log("Does NodeRC contain a directory at '/'? = %s", noderc.isDirectory("/"));
-console.log("Does NodeRC contain a directory at '.'? = %s", noderc.isDirectory("."));
+  if(noderc.exists(tst)) {
 
-console.log("Does NodeRC contain a file or directory named 'tst.txt'? = %s",   noderc.exists("tst.txt"));
-console.log("Does NodeRC contain a file or directory named '/tst.txt'? = %s",   noderc.exists("/tst.txt"));
+    if(noderc.isFile(tst)) {
 
-console.log("Contents of 'tst.txt' = \n%s", noderc.open("tst.txt"))
+      // Do file functions...
+      console.log("Contents of '%s' = \n%s", tst, noderc.open(tst))
+      return true;
+
+    }
+
+    if(noderc.isDirectory(tst)) {
+
+      // Do directory functions...
+      console.log("Entry '%s' is a directory", tst)
+      return true;
+    }
+
+    console.log("Entry exists, but is neither a file nor a directory... this should never happen.")
+    return false;
+  }
+
+  console.log("Entry '%s' does not exist.", tst)
+  return false;
+
+}
+
+const tstA = "tst.txt";
+const resA = doTest(tstA);
+
+const tstB = "sts.xtx";
+const resB = doTest(tstB);
+
+const tstC = "/";
+const resC = doTest(tstC);
+
+const tstD = "/usr";
+const resD = doTest(tstD);
+
+const tstE = "/tst.txt";
+const resE = doTest(tstE);
