1aa60f7f794955183cb1f59241f0d78ebdb2b35c
StoneyDSP <nathanjhood@googlemail.com>
2024-01-11 07:27:11 +0100

    Enhanced RESOURCES management
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/noderc/version.hpp
+++ include/noderc/version.hpp
@@ -1,4 +1,4 @@
-// noderc v1.0.0.b7a3f241158d2d4efa0078b4aba39f118171cb5e
+// noderc v1.0.0.eeb2c55467a57085bef2fc33392f4baf4f9344fa
 // Version control info
 
 #ifndef NODERC_VERSION_H_
@@ -7,8 +7,8 @@
 #define NODERC_VERSION_MAJOR "1"
 #define NODERC_VERSION_MINOR "0"
 #define NODERC_VERSION_PATCH "0"
-#define NODERC_VERSION_TWEAK "b7a3f241158d2d4efa0078b4aba39f118171cb5e"
-#define NODERC_VERSION "1.0.0.b7a3f241158d2d4efa0078b4aba39f118171cb5e"
+#define NODERC_VERSION_TWEAK "eeb2c55467a57085bef2fc33392f4baf4f9344fa"
+#define NODERC_VERSION "1.0.0.eeb2c55467a57085bef2fc33392f4baf4f9344fa"
 #define NODERC_REPOSITORY "https://github.com/nathanjhood/noderc.git"
 
 #endif // NODERC_VERSION_H_
