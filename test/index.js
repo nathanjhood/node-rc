3090928f09be8cf7200209d83cc9653ac9de8bb7
StoneyDSP <nathanjhood@googlemail.com>
2024-01-11 02:25:37 +0100

    Improved tests
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/index.js
+++ test/index.js
@@ -5,30 +5,27 @@ console.log(noderc.version());
 
 function doTest(tst) {
 
-  if(noderc.exists(tst)) {
-
-    if(noderc.isFile(tst)) {
-
-      // Do file functions...
-      console.log("Contents of '%s' = \n%s", tst, noderc.open(tst))
-      return true;
-
-    }
-
-    if(noderc.isDirectory(tst)) {
-
-      // Do directory functions...
-      console.log("Entry '%s' is a directory", tst)
-      return true;
-    }
-
-    console.log("Entry exists, but is neither a file nor a directory... this should never happen.")
+  if(!noderc.exists(tst)) {
+    console.log("Entry '%s' does not exist.", tst)
     return false;
   }
 
-  console.log("Entry '%s' does not exist.", tst)
-  return false;
+  if(noderc.isFile(tst)) {
 
+    // Do file functions...
+    console.log("Contents of '%s' = \n%s", tst, noderc.open(tst))
+    return true;
+  }
+
+  if(noderc.isDirectory(tst)) {
+
+    // Do directory functions...
+    console.log("Entry '%s' is a directory", tst)
+    return true;
+  }
+
+  console.log("Entry exists, but is neither a file nor a directory... this should never happen.")
+  return false;
 }
 
 const tstA = "tst.txt";
