58f0857a33c601b37c8ba132e669ce35f2a22340
StoneyDSP <nathanjhood@googlemail.com>
2024-01-11 05:32:38 +0100

    Improved linting standards
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/noderc/version.hpp
+++ include/noderc/version.hpp
@@ -1,4 +1,4 @@
-// noderc v1.0.0.75fd15f09e800e6858250a637aa4ce87ff2cbdb4
+// noderc v1.0.0.9ba265aa73292a3ef95c46988f4fc29594f2bb00
 // Version control info
 
 #ifndef NODERC_VERSION_H_
@@ -7,8 +7,8 @@
 #define NODERC_VERSION_MAJOR "1"
 #define NODERC_VERSION_MINOR "0"
 #define NODERC_VERSION_PATCH "0"
-#define NODERC_VERSION_TWEAK "75fd15f09e800e6858250a637aa4ce87ff2cbdb4"
-#define NODERC_VERSION "1.0.0.75fd15f09e800e6858250a637aa4ce87ff2cbdb4"
+#define NODERC_VERSION_TWEAK "9ba265aa73292a3ef95c46988f4fc29594f2bb00"
+#define NODERC_VERSION "1.0.0.9ba265aa73292a3ef95c46988f4fc29594f2bb00"
 #define NODERC_REPOSITORY "https://github.com/nathanjhood/noderc.git"
 
 #endif // NODERC_VERSION_H_
--- src/noderc/noderc.cpp
+++ src/noderc/noderc.cpp
@@ -1,9 +1,9 @@
 /**
  * @file addon.cpp
  * @author Nathan J. Hood (nathanjood@googlemail.com)
- * @brief Exports 'base64' as a binary NodeJS addon.
+ * @brief Exports 'noderc' as a binary NodeJS addon.
  * @version 0.1
- * @date 2024-01-03
+ * @date 2024-01-09
  *
  * @copyright Copyright (c) 2024 Nathan J. Hood (nathanjood@googlemail.com)
  *
@@ -19,21 +19,28 @@
 #include <iostream>
 #include <iterator>
 
+// Get Node Addon API
 #include <napi.h>
 
+// Get CMakeRC
 #include <cmrc/cmrc.hpp>
 
+// Register our resource library namespace
 CMRC_DECLARE(noderc::resources);
 
-namespace noderc {
+namespace noderc
+{
 
-namespace addon {
+namespace addon
+{
 
-Napi::Value Hello(const Napi::CallbackInfo& info) {
+Napi::Value Hello(const Napi::CallbackInfo& info)
+{
   return Napi::String::New(info.Env(), "noderc is online!");
 }
 
-Napi::Value Version(const Napi::CallbackInfo& info) {
+Napi::Value Version(const Napi::CallbackInfo& info)
+{
   return Napi::String::New(info.Env(), NODERC_VERSION);
 }
 
@@ -75,8 +82,10 @@ Napi::Value Open(const Napi::CallbackInfo& args)
     out = Napi::String::New(env, std::string(chunk.begin(), chunk.end()));
 
   } catch (const std::system_error& e) {
+
     // If there was an error...
-    if (e.code() == std::errc::no_such_file_or_directory) {
+    if (e.code() == std::errc::no_such_file_or_directory)
+    {
       std::string message(e.what());
       message += '\n';
       message += "noderc: No such file or directory: ";
@@ -85,6 +94,7 @@ Napi::Value Open(const Napi::CallbackInfo& args)
       message.clear();
       return env.Null();
     }
+
     std::string message(e.what());
     message += '\n';
     message += args[0].As<Napi::String>();
@@ -96,8 +106,8 @@ Napi::Value Open(const Napi::CallbackInfo& args)
   return out;
 }
 
-Napi::Value IsFile(const Napi::CallbackInfo& args) {
-
+Napi::Value IsFile(const Napi::CallbackInfo& args)
+{
   Napi::Env env = args.Env();
 
   // Arguments required: one only
@@ -119,8 +129,8 @@ Napi::Value IsFile(const Napi::CallbackInfo& args) {
   return Napi::Boolean::New(env, fs.is_file(args[0].ToString().Utf8Value()));
 }
 
-Napi::Value IsDirectory(const Napi::CallbackInfo& args) {
-
+Napi::Value IsDirectory(const Napi::CallbackInfo& args)
+{
   Napi::Env env = args.Env();
 
   // Arguments required: one only
@@ -142,8 +152,8 @@ Napi::Value IsDirectory(const Napi::CallbackInfo& args) {
   return Napi::Boolean::New(env, fs.is_directory(args[0].ToString().Utf8Value()));
 }
 
-Napi::Value Exists(const Napi::CallbackInfo& args) {
-
+Napi::Value Exists(const Napi::CallbackInfo& args)
+{
   Napi::Env env = args.Env();
 
   // Arguments required: one only
@@ -170,10 +180,17 @@ Napi::Value Diff(const Napi::CallbackInfo& args) {
 
   Napi::Env env = args.Env();
 
-  // Arguments required: one only
+  // Arguments required: two only
   if (args.Length() != 2)
   {
     Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  // Params must be strings
+  if (!args[0].IsString() || !args[1].IsString())
+  {
+    Napi::TypeError::New(env, "Wrong argument type!").ThrowAsJavaScriptException();
     return env.Null();
   }
 
@@ -182,28 +199,32 @@ Napi::Value Diff(const Napi::CallbackInfo& args) {
 
   std::ifstream arg0_fs{arg0.data(), std::ios_base::binary};
 
-  if (!arg0_fs) {
+  if (!arg0_fs)
+  {
     Napi::TypeError::New(env, "Invalid filename").ThrowAsJavaScriptException();
     return env.Null();
   }
 
   using iter         = std::istreambuf_iterator<char>;
   const auto fs_size = std::distance(iter(arg0_fs), iter());
-  arg0_fs.seekg(0);
-
   auto       fs      = cmrc::noderc::resources::get_filesystem();
 
-  if (!fs.exists(arg1)) {
+  arg0_fs.seekg(0);
+
+  if (!fs.exists(arg1))
+  {
     Napi::TypeError::New(env, "Invalid filename: Does not exist").ThrowAsJavaScriptException();
     return env.Null();
   }
 
-  if (fs.is_directory(arg1)) {
+  if (fs.is_directory(arg1))
+  {
     Napi::TypeError::New(env, "Invalid filename: Is a directory").ThrowAsJavaScriptException();
     return env.Null();
   }
 
-  if (!fs.is_file(arg1)) {
+  if (!fs.is_file(arg1))
+  {
     Napi::TypeError::New(env, "Invalid filename: Is not a file").ThrowAsJavaScriptException();
     return env.Null();
   }
@@ -211,12 +232,14 @@ Napi::Value Diff(const Napi::CallbackInfo& args) {
   auto       arg1_rc   = fs.open(arg1);
   const auto rc_size   = std::distance(arg1_rc.begin(), arg1_rc.end());
 
-  if (rc_size != fs_size) {
+  if (rc_size != fs_size)
+  {
     std::cerr << "File sizes do not match: FS == " << fs_size << ", RC == " << rc_size << "\n";
     return Napi::Boolean::New(env, false);
   }
 
-  if (!std::equal(arg1_rc.begin(), arg1_rc.end(), iter(arg0_fs))) {
+  if (!std::equal(arg1_rc.begin(), arg1_rc.end(), iter(arg0_fs)))
+  {
     std::cerr << "File contents do not match\n";
     return Napi::Boolean::New(env, false);
   }
@@ -227,8 +250,8 @@ Napi::Value Diff(const Napi::CallbackInfo& args) {
 
 
 // Construct an 'initializer' object that carries our functions
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
   // Export a chosen C++ function under a given Javascript key
   exports.Set(
     Napi::String::New(env, "hello"), // Name of function on Javascript side...
@@ -278,7 +301,7 @@ NODE_API_MODULE(noderc, Init) // (name to use, initializer to use)
 
 } // namespace addon
 
-// Here, we can extend the namepsace with Napi overloads.
+// Here, we can extend the namepsace with Napi overloads, if we need them.
 
 } // namespace noderc
 
