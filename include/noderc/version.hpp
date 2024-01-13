5e8a16abf52557033075c38d65a1dfd98b5e68e2
StoneyDSP <nathanjhood@googlemail.com>
2024-01-13 19:12:52 +0100

    version increment
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/noderc/version.hpp
+++ include/noderc/version.hpp
@@ -1,4 +1,4 @@
-// noderc v1.0.0.ffc06cca25deae3a6fc771a10c10d4c097dc35b3
+// noderc v1.0.0.61c0eff43218721108bc934b452fc06ad51887b5
 // Version control info
 
 #ifndef NODERC_VERSION_H_
@@ -7,8 +7,8 @@
 #define NODERC_VERSION_MAJOR "1"
 #define NODERC_VERSION_MINOR "0"
 #define NODERC_VERSION_PATCH "0"
-#define NODERC_VERSION_TWEAK "ffc06cca25deae3a6fc771a10c10d4c097dc35b3"
-#define NODERC_VERSION "1.0.0.ffc06cca25deae3a6fc771a10c10d4c097dc35b3"
+#define NODERC_VERSION_TWEAK "61c0eff43218721108bc934b452fc06ad51887b5"
+#define NODERC_VERSION "1.0.0.61c0eff43218721108bc934b452fc06ad51887b5"
 #define NODERC_REPOSITORY "https://github.com/nathanjhood/noderc.git"
 
 #endif // NODERC_VERSION_H_
